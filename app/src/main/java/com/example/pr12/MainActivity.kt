1ce1f177467b61cf72bdb0b221871679c5942878
ParadaSergei <sergejparada95@gmail.com>
2022-10-26 20:34:19 +0300

    pr12

--- app/src/main/java/com/example/pr12/MainActivity.kt
+++ app/src/main/java/com/example/pr12/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.pr12
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageView
-import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    val check1:CheckBox = findViewById(R.id.checkBox)
+    val check2:CheckBox = findViewById(R.id.checkBox2)
+    val check3:CheckBox = findViewById(R.id.checkBox3)
+    var bool:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         val description:String = "Pear"
         photo.setImageResource(image)
         photo.setContentDescription(description)
-
     }
     fun grape(view: View){
         val photo:ImageView = findViewById(R.id.imageView)
@@ -36,27 +39,34 @@ class MainActivity : AppCompatActivity() {
         photo.setContentDescription(description)
     }
     fun pearCheck(view: View){
-            val photo:ImageView = findViewById(R.id.imageView5)
-            val image:Int = R.drawable.pear
-            val description:String = "Pear"
-            photo.setImageResource(image)
-            photo.setContentDescription(description)
+        var imageView4:ImageView = findViewById(R.id.imageView4)
 
+    if (check1.isChecked == true){
+    imageView4.setVisibility(View.VISIBLE)
+}
+        else{
+    imageView4.setVisibility(View.GONE)
+
+        }
+    }
+    fun grapeCheck(view: View) {
+        var imageView3:ImageView = findViewById(R.id.imageView3)
+        if (check2.isChecked == true){
+            imageView3.setVisibility(View.VISIBLE)
+        }
+        else{
+            imageView3.setVisibility(View.GONE)
+        }
     }
     fun appleCheck(view: View){
+        var imageView2:ImageView = findViewById(R.id.imageView2)
+        if (check3.isChecked == true){
+            imageView2.setVisibility(View.VISIBLE)
+        }
+        else{
+            imageView2.setVisibility(View.GONE)
+        }
 
-            val photo:ImageView = findViewById(R.id.imageView4)
-            val image:Int = R.drawable.apple
-            val description:String = "Apple"
-            photo.setImageResource(image)
-            photo.setContentDescription(description)
-    }
-    fun grapeCheck(view: View){
-            val photo:ImageView = findViewById(R.id.imageView3)
-            val image:Int = R.drawable.grape
-            val description:String = "Grape"
-            photo.setImageResource(image)
-            photo.setContentDescription(description)
 
-    }
-}
+
+}}
