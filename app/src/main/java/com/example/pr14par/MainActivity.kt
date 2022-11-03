d129b09c316f4eb1607ca4565d4656f43f5901f3
Pasha <test@example.com>
2022-11-03 13:00:35 +0300

    pr14

---
+++ app/src/main/java/com/example/pr14par/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.pr14par
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val argument : Bundle? = intent.extras
+        var emailText : EditText = findViewById(R.id.emailEditText)
+        var nameText : EditText = findViewById(R.id.nameEditText)
+        var ageText : EditText = findViewById(R.id.ageTextView)
+        if (argument != null) {
+            emailText.setText(argument.get("email").toString())
+            nameText.setText(argument.get("name").toString())
+            ageText.setText(argument.get("age").toString())
+        }
+    }
+    fun transition(view: View) {
+        val emailText: EditText = findViewById(R.id.emailEditText)
+        val nameText: EditText = findViewById(R.id.nameEditText)
+        val ageText: EditText = findViewById(R.id.ageTextView)
+        val intent = Intent(this@MainActivity, userinfo::class.java)
+        if (emailText.text.isNotEmpty() && nameText.text.isNotEmpty() && ageText.text.isNotEmpty()) {
+            intent.putExtra("email", emailText.text.toString())
+            intent.putExtra("name", nameText.text.toString())
+            intent.putExtra("age", ageText.text.toString())
+            startActivity(intent)
+        } else {
+            val toast: Toast = Toast.makeText(applicationContext, "Поле пустое!", Toast.LENGTH_LONG)
+            toast.show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pr14par/userinfo.kt
@@ -0,0 +1,54 @@
+package com.example.pr14par
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+
+class userinfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_userinfo)
+        val argument : Bundle? = intent.extras
+        var emailText : EditText = findViewById(R.id.emailEditText)
+        var nameText : EditText = findViewById(R.id.nameEditText)
+        var ageText : EditText = findViewById(R.id.ageTextNumber)
+        if (argument != null) {
+            emailText.setText(argument.get("email").toString())
+            nameText.setText(argument.get("name").toString())
+            ageText.setText(argument.get("age").toString())
+        }
+    }
+
+    fun transitions(view: View) {
+        val argument : Bundle? = intent.extras
+        var emailText : EditText = findViewById(R.id.emailEditText)
+        var nameText : EditText = findViewById(R.id.nameEditText)
+        var ageText : EditText = findViewById(R.id.ageTextNumber)
+        val intent = Intent(this@userinfo, MainActivity::class.java)
+        if(emailText.text.isNotEmpty() && nameText.text.isNotEmpty() && ageText.text.isNotEmpty())
+        {
+            if (argument != null) {
+                if(emailText.text.toString() != argument.get("email").toString()) intent.putExtra("email", "* ${emailText.text.toString()}")
+                else intent.putExtra("email", emailText.text.toString())
+
+                if(nameText.text.toString() != argument.get("name").toString()) intent.putExtra("name", "* ${nameText.text.toString()}")
+                else intent.putExtra("name", nameText.text.toString())
+
+                if(ageText.text.toString() != argument.get("age").toString()) intent.putExtra("age", "* ${ageText.text.toString()}")
+                else intent.putExtra("age", ageText.text.toString())
+                startActivity(intent)
+            }
+        }
+        else {
+            val toast: Toast = Toast.makeText(applicationContext, "Поле пустое!", Toast.LENGTH_LONG)
+            toast.show()
+        }
+    }
+}
